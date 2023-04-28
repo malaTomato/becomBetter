8f6ebc050137bd2ad17494a574d70e7d5f6bb55c
Willson Xiong <willson.xiong@wearecgl.com>
2023-04-28 17:10:04 +0800

    feat: jvm

---
+++ jvm/src/main/java/org/bear/Main.java
@@ -0,0 +1,15 @@
+package org.bear;
+
+/**
+ * [${PROJECT_NAME}]
+ * [org.bear]
+ * [${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}]
+ *
+ * @author [willson.xiong]
+ * @version 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
